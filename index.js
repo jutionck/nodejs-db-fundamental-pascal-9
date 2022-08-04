6d33cd2fe033ac9f5acf76197404b5a945e9b859
Jution Candra Kirana <jutionck@gmail.com>
2022-08-04 21:58:29 +0700

    update: mapping result sql to object

--- index.js
+++ index.js
@@ -7,12 +7,32 @@ const employeeUseCase = EmployeeUseCase(EmployeeRepository());
 
 const get = async (req, res) => {
     const employees = await employeeUseCase.getAllEmployee();
-    res.end(JSON.stringify(employees.rows));
+    const employee = [];
+    for (let i = 0;i < employees.rows.length; i++) {
+        employee.push({
+            id: employees.rows[i].id,
+            firstName: employees.rows[i].first_name,
+            lastName: employees.rows[i].last_name,
+            dob: employees.rows[i].dob,
+            pob: employees.rows[i].pob,
+            address: employees.rows[i].address,
+        });
+    }
+    res.end(JSON.stringify(employee));
 }
 
 const getById = async (req, res, id) => {
     const employee = await employeeUseCase.getEmployeeById(id.id);
-    res.end(JSON.stringify(employee.rows[0]));
+    const emp = {};
+    for (let i = 0;i < employee.rows.length; i++) {
+        emp.id =  employee.rows[i].id;
+        emp.firstName =  employee.rows[i].first_name;
+        emp.lastName = employee.rows[i].last_name;
+        emp.dob = employee.rows[i].dob;
+        emp.pob = employee.rows[i].pob;
+        emp.address = employee.rows[i].address;
+    }
+    res.end(JSON.stringify(emp));
 }
 
 const create = async (req, res, employee) => {
