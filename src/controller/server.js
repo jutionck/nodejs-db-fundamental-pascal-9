1d01d675f33fcbfff876253aca0ef927203b14b1
Jution Candra Kirana <jutionck@gmail.com>
2022-08-05 14:09:06 +0700

    feat: add midleware

--- src/controller/server.js
+++ src/controller/server.js
@@ -1,32 +1,38 @@
 const http = require("http");
 const port = 8181;
 const { get, getById, create, update, deleteEmp } = require('./employee.controller');
+const {Response} = require("../utils/response");
 
 const run = () => {
     const server = http.createServer((request, response) => {
         response.setHeader('Content-Type', 'application/json');
-        if (request.method === 'GET' && request.url === '/employee') {
-            get(request, response).catch();
-        } else if (request.method === 'POST' && request.url === '/employee') {
-            request.addListener('data', (data) => {
-                const body = JSON.parse(data);
-                create(request, response, body).catch();
-            });
-        } else if (request.method === 'GET' && request.url === '/employee/id') {
-            request.addListener('data', (data) => {
-                const body = JSON.parse(data);
-                getById(request, response, body).catch();
-            });
-        } else if (request.method === 'PUT' && request.url === '/employee') {
-            request.addListener('data', (data) => {
-                const body = JSON.parse(data);
-                update(request, response, body).catch();
-            });
-        } else if (request.method === 'DELETE' && request.url === '/employee') {
-            request.addListener('data', (data) => {
-                const body = JSON.parse(data);
-                deleteEmp(request, response, body).catch();
-            });
+        // Set on Header = Token = enigma
+        if (!request.headers.token) {
+            response.end(JSON.stringify(Response().errorMessage(response.statusCode = 401, 'Unauthorized')));
+        } else {
+            if (request.method === 'GET' && request.url === '/employee') {
+                get(request, response).catch();
+            } else if (request.method === 'POST' && request.url === '/employee') {
+                request.addListener('data', (data) => {
+                    const body = JSON.parse(data);
+                    create(request, response, body).catch();
+                });
+            } else if (request.method === 'GET' && request.url === '/employee/id') {
+                request.addListener('data', (data) => {
+                    const body = JSON.parse(data);
+                    getById(request, response, body).catch();
+                });
+            } else if (request.method === 'PUT' && request.url === '/employee') {
+                request.addListener('data', (data) => {
+                    const body = JSON.parse(data);
+                    update(request, response, body).catch();
+                });
+            } else if (request.method === 'DELETE' && request.url === '/employee') {
+                request.addListener('data', (data) => {
+                    const body = JSON.parse(data);
+                    deleteEmp(request, response, body).catch();
+                });
+            }
         }
     });
 
